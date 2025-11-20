b63750d77b8ed428ecc04d3ea13ee6766040857d
jichang-lee <jccc23@naver.com>
2025-11-20 16:51:12 +0900

    jwtUtil 구성중

---
+++ src/main/java/com/jwt/security/config/JwtUtil.java
@@ -0,0 +1,37 @@
+package com.jwt.security.config;
+
+import com.jwt.security.domain.User;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey;
+    private final long accessTokenExpiration;
+    private final long refreshTokenExpiration;
+
+    public JwtUtil(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.access_expiration_time}") long accessTokenExpiration,
+            @Value("${jwt.refresh_expiration_time}") long refreshTokenExpiration
+    ) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.accessTokenExpiration = accessTokenExpiration;
+        this.refreshTokenExpiration = refreshTokenExpiration;
+    }
+
+    public String generateAccessToken(String email, String role) {
+        return null;
+    }
+
+    public String generateRefreshToken(String email) {
+        return null;
+    }
+}
