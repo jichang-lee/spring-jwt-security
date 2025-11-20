76ae9c547944bc867ca5afd453c40457b773d5c5
jichang-lee <jccc23@naver.com>
2025-11-20 15:52:09 +0900

    yml 설정, jwt 추가

---
+++ src/main/java/com/jwt/security/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.jwt.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "hi";
+    }
+}
