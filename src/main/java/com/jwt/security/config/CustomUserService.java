d5a5de7166325ffd265b2f8a2537b0d50ef5d434
jichang-lee <jccc23@naver.com>
2025-12-25 18:53:08 +0900

    CustomUserService 추가

---
+++ src/main/java/com/jwt/security/config/CustomUserService.java
@@ -0,0 +1,25 @@
+package com.jwt.security.config;
+
+import com.jwt.security.domain.User;
+import com.jwt.security.exception.UserNotFound;
+import com.jwt.security.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(UserNotFound::new);
+
+        return new CustomUserDetails(user);
+    }
+}
